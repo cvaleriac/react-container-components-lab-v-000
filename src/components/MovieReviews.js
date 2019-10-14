7b6cec9880fc911bb3d484cd22d8e7be64beef8b
valeria campilongo <cvaleriac@hotmail.it>
2019-10-14 14:55:24 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,16 @@
 // Code MovieReviews Here
+const MovieReviews = ({reviews}) => {
+  return (
+    <ul className="review-list">
+      {reviews.map((review, index) =>
+      <li className="review" key={index}>
+      {review.display_title}</li>
+    )}
+      </ul>)
+}
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
+
+export default MovieReview
